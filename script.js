a26c22a7db10d55816ba0c987aff59bf2706090f
Nilde Lima <nildelima@ThinkBook15G2ITL5c58e893.lan>
2022-03-18 11:03:55 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,102 @@
+
+// 1 -Que tal uma dificuldade um pouco maior? Imprima todos os números entre 30 e 40 (inclusive 30 e 40), porém os números 33 e 37 não devem ser impressos! No final escreva a palavra "FIM". Use o while para essa tarefa. DICA: o if pode ajudá-lo!
+// let contador = 30;
+
+// while(contador <= 40){
+// if(contador === 33){
+// contador = contador +1;
+// }
+// else if(contador === 37){
+//     contador = contador +1;
+// } else {
+//     document.write(contador + (" "));
+//     contador = contador +1;
+// }
+// }
+//-----------------------------------------------------------------------
+
+// let contador = 20;
+
+// while(contador>0){
+//     if(contador ===13){
+//         contador = contador -1;
+//     }
+// document.write (contador + "<br>")
+// contador = contador -1;
+// }
+
+
+    // function pulaLinha() {
+    //     document.write("<br>");
+    // }
+
+    // function mostra(frase) {
+
+    //     document.write(frase);
+    //     pulaLinha();
+    // }
+
+    // var anoCopa = 1930;
+    // var limite = parseInt(prompt("Qual é o ano limite?"));
+
+    // while(anoCopa <= limite) {
+
+    //     mostra(anoCopa + " tem copa!");
+    //     anoCopa = anoCopa + 4;
+    // }
+
+
+
+    // mostra("Ufa! Esses foram os anos de copa até " + limite);
+
+    
+
+//     var loginCadastrado = "alura";
+//     var senhaCadastrada = "alura321";
+
+//     var maximoTentativas = 3;
+//     var tentativaAtual = 1;
+
+//     while(tentativaAtual <= maximoTentativas){
+
+//     var loginInformado = prompt("Informe seu login");
+//     var senhaInformada = prompt("Informe sua senha");
+
+//     if( loginCadastrado == loginInformado && senhaCadastrada == senhaInformada ) {
+
+//         alert("Bem-vindo ao sistema " + loginInformado);
+//         tentativaAtual = maximoTentativas;
+
+//     } else {
+
+//         if (tentativaAtual == maximoTentativas) {
+//             alert("Número permitido de tentativas ultrapassado!");
+//         } else {
+//             alert("Login inválido. Tente novamente");
+//         }
+//     }
+//     tentativaAtual += 1;
+// }
+
+// ....................................................................
+
+// var idade = parseInt(prompt("Digite sua idade"));
+
+// while(isNaN(idade)) {
+//     idade = parseInt(prompt("Digite sua idade"));    
+// }
+
+// alert(idade);
+
+// var tabuada = 7;
+
+// for(var i = 1; i <= 10; i +=1){
+//     document.write(tabuada*[i] + ("<br>"));
+
+// }
+
+
+
+
+    
+
